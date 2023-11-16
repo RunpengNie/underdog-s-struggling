40b91f7e41f48c763d7bbed68b84f03fc4ff76ac
Tony Liu <31741598+TonyLiu836@users.noreply.github.com>
2023-11-15 18:42:19 -0800

    Change order of table creation. Modify according to 11.1-Prelim PR.

--- Backend/Underdogs-Struggling/src/main/sql/DatabaseTables.sql
+++ Backend/Underdogs-Struggling/src/main/sql/DatabaseTables.sql
@@ -1,3 +1,20 @@
+-- Roless: Store roles information
+--     RoleID (PK)
+--     RoleName
+CREATE TABLE Roles(
+    RoleID INT,
+    RoleName VARCHAR(20),
+    PRIMARY KEY (RoleID)
+);
+
+-- Tags: Store different tags
+--     TagID (PK)
+--     TagName
+CREATE TABLE Tags(
+    TagID INT AUTO_INCREMENT,
+    TagName VARCHAR(20),
+    Primary Key (TagID)
+);
 
 -- Users: Store user information
 --     UserID (PK)
@@ -6,12 +23,18 @@
 --     Role (FK, to Roles.RoleID)
 --     Email (Unique)
 --     RegistrationDate
-CREATE TABLE Users(UserID INT, Username VARCHAR(20) UNIQUE, Password VARCHAR(20) NOT NULL, Role INT , Email VARCHAR(255) UNIQUE, RegistrationDate Datetime, PRIMARY KEY (UserID), FOREIGN KEY (Roles) REFERENCES Role(ID));
+CREATE TABLE Users(
+    UserID INT UNIQUE,
+    Username VARCHAR(20) UNIQUE,
+    Password VARCHAR(20) NOT NULL,
+    Role INT,
+    Email VARCHAR(255) UNIQUE,
+    RegistrationDate Datetime,
+    PRIMARY KEY (UserID),
+    FOREIGN KEY (Role) REFERENCES Roles (RoleID)
+);
 
--- Roless: Store roles information
---     RoleID (PK)
---     RoleName
-CREATE TABLE Roles(RoleID INT, RoleName VARCHAR(20), PRIMARY KEY (RoleID));
+
 
 -- Entries: Store knowledge entries
 --     EntryID (PK)
@@ -23,23 +46,42 @@ CREATE TABLE Roles(RoleID INT, RoleName VARCHAR(20), PRIMARY KEY (RoleID));
 --     CreatedDate
 --     LastModifyDate
 --     KnowledgeLevel
-CREATE TABLE Entries(EntryID INT, Title VARCHAR(50) UNIQUE, Topic VARCHAR(255), Index INT, Content TEXT,
-AuthorID INT, CreatedDate Datetime, LastModified DateTime, KnowledgeLevel INT, PRIMARY KEY(EntryID), FOREIGN KEY(AuthorID) REFERENCES Users(UserID));
+CREATE TABLE Entries(
+    EntryID INT AUTO_INCREMENT,
+    Title VARCHAR(50) UNIQUE,
+    Topic VARCHAR(255),
+    PositionIndex INT,
+    Content TEXT,
+    AuthorID INT,
+    CreatedDate Datetime,
+    LastModified DateTime,
+    KnowledgeLevel INT,
+    PRIMARY KEY (EntryID),
+    FOREIGN KEY (AuthorID) REFERENCES Users (UserID)
+);
 
 -- ModificationRecords: Store modification records of entries
 --     RecordID (PK)
 --     UserID (FK, to Users.UserID)
 --     ModificationTimestamp
-CREATE TABLE ModificationRecords(RecordID INT, UserID INT, ModificationTimestamp Datetime, PRIMARY KEY(RecordID), FOREIGN KEY(UserID) REFERENCES Users(UserID));
+CREATE TABLE ModificationRecords(
+    RecordID INT AUTO_INCREMENT,
+    UserID INT,
+    ModificationTimestamp Datetime,
+    PRIMARY KEY(RecordID),
+    FOREIGN KEY(UserID) REFERENCES Users (UserID)
+);
 
--- Tags: Store different tags
---     TagID (PK)
---     TagName
-CREATE TABLE Tags(TagID INT, TagName VARCHAR(20));
 
 -- EntryTags: Store many-to-many relationships between entries and tags
 --     EntryTagID (PK)
 --     EntryID (FK, to Entries.EntryID)
 --     TagID (FK, to Tags.TagID)
-CREATE TABLE EntryTags(EntryTagID INT, EntryID INT, TagID INT, PRIMARY KEY(EntryTagID), FOREIGN KEY(EntryID) REFERENCES Entries(EntryID), FOREIGN KEY(TagID) REFERENCES Tags(TagID));
-
+CREATE TABLE EntryTags(
+    EntryTagID INT,
+    EntryID INT,
+    TagID INT,
+    PRIMARY KEY(EntryTagID),
+    FOREIGN KEY(EntryID) REFERENCES Entries (EntryID),
+    FOREIGN KEY(TagID) REFERENCES Tags (TagID)
+);
