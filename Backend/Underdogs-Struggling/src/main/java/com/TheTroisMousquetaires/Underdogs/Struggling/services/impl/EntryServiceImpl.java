e55691b10a836e8fcb07f555fe47a3f98fefc852
zhouzhou918@163.com <zhouzhou918@163.com>
2024-08-11 14:47:15 -0500

    Fix doc service impl for alignment

--- Backend/Underdogs-Struggling/src/main/java/com/TheTroisMousquetaires/Underdogs/Struggling/services/impl/EntryServiceImpl.java
+++ Backend/Underdogs-Struggling/src/main/java/com/TheTroisMousquetaires/Underdogs/Struggling/services/impl/EntryServiceImpl.java
@@ -7,7 +7,6 @@ import com.TheTroisMousquetaires.Underdogs.Struggling.services.EntryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
